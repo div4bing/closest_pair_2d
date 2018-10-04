ef767410e18abb7b003d8437854b005941751151
div4bing <div4bing>
2018-10-03 23:45:06 -0400

    2D and 3D combined into one file with macro to enable either

--- closestPair.c
+++ closestPair.c
@@ -2,24 +2,40 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define DEBUG_ENABLE 0    // Enable or Disable the Debug logs
+#define DIMENSION_3D 1	  // Enable to have 3D closest pair of points, If 0 - means input is 2D
 
 // Global declaration
-struct point_t
-{
-  long long x, y;
-};
+
+#if DIMENSION_3D	 // 3-Dimensional Enabled
+	struct point_t      // 3-Dimensional Coordinate Point
+	{
+	  long long x, y, z;
+	};
+#else
+	struct point_t
+	{
+	  long long x, y;
+	};
+#endif
 
 typedef struct point_t point;
 
 float calculateDistance(point a, point b);
 int qsortCompX (const void *arg1, const void *arg2);
 int qsortCompY (const void *arg1, const void *arg2);
+#if DIMENSION_3D	 // 3-Dimensional Enabled
+	int qsortCompZ (const void *arg1, const void *arg2);
+#endif
 float findClosestPair(point p[], long long size, point *A, point *B);
 float findMin(float L, float R, int *findMinFlag);
 float findMinInCenter(point p[], long long mid, float dminLR, long long size, point *A, point *B);
 long long getTotalLines(FILE *fp);
 long long totalLines;
+#if DIMENSION_3D	 // 3-Dimensional Enabled
+long long searchIndex(point p[], long long size, long long valX, long long valY, long long valZ);
+#else
+long long searchIndex(point p[], long long size, long long valX, long long valY);
+#endif
 
 //**************** Print Point Values for Debug *******************
 void printPoints(point p[], long long size);
@@ -28,7 +44,11 @@ void printPoints(point p[], long long size)
   printf("Print Points: Size: %lld\n", size);
   for(long long i = 0; i< size; i++)
   {
+#if DIMENSION_3D	 // 3-Dimensional Enabled
+    printf("(%lld, %lld, %lld)\n", p[i].x, p[i].y, p[i].z);
+#else
     printf("(%lld, %lld)\n", p[i].x, p[i].y);
+#endif
   }
 }
 //*************************************************
@@ -49,7 +69,11 @@ float findMin(float L, float R, int *findMinFlag)
 
 float calculateDistance(point a, point b)
 {
+#if DIMENSION_3D	 // 3-Dimensional Enabled
+  return sqrt(((a.x - b.x)*(a.x - b.x)) + ((a.y - b.y)*(a.y - b.y)) + ((a.z - b.z)*(a.z - b.z)));
+#else
   return sqrt(((a.x - b.x)*(a.x - b.x))+((a.y - b.y)*(a.y - b.y)));
+#endif
 }
 
 //qsort(p, size, sizeof( point), qsortCompX);
@@ -63,6 +87,14 @@ int qsortCompY (const void *arg1, const void *arg2)
 {
    return((int)(( point *)arg1)->y - (( point *)arg2)->y);
 }
+
+#if DIMENSION_3D	 // 3-Dimensional Enabled
+//qsort(p, size, sizeof( point), qsortCompZ);
+int qsortCompZ (const void *arg1, const void *arg2)
+{
+   return((int)(( point *)arg1)->z - (( point *)arg2)->z);
+}
+#endif
 
 float findMinInCenter(point p[], long long mid, float dminLR, long long size, point *A, point *B)
 {
@@ -84,19 +116,20 @@ float findMinInCenter(point p[], long long mid, float dminLR, long long size, po
 
   sizeCenter = count;
 
+#if DIMENSION_3D	 // 3-Dimensional Enabled
+  qsort(pCenter, sizeCenter, sizeof( point), qsortCompZ);   // Sort the center part with Z- coordinate
+#else
   qsort(pCenter, sizeCenter, sizeof( point), qsortCompY);   // Sort the center part with Y- coordinate
-
-#if DEBUG_ENABLE                    // Print Only if Debug enabled
-  printf("After Y Sort: dminCenter-> %f\n", dminCenter);
-  for(i = 0; i< sizeCenter; i++)
-  {
-    printf("(%lld, %lld)\n", pCenter[i].x, pCenter[i].y);
-  }
 #endif
 
+  // We will only try to find the points from particular point which is less than dminCenter (the rectangle)
   for (i = 0; i < sizeCenter; i++)
   {
+#if DIMENSION_3D	 // 3-Dimensional Enabled
+    for (j = i+1; (j < sizeCenter) && ((pCenter[j].z - pCenter[i].z) < dminCenter); j++)
+#else
     for (j = i+1; (j < sizeCenter) && ((pCenter[j].y - pCenter[i].y) < dminCenter); j++)
+#endif
     {
       tempDist = calculateDistance(pCenter[i], pCenter[j]);
       if (tempDist < dminCenter)
@@ -104,18 +137,10 @@ float findMinInCenter(point p[], long long mid, float dminLR, long long size, po
         dminCenter = tempDist;
         *A = pCenter[i];
         *B = pCenter[j];
-#if DEBUG_ENABLE                    // Print Only if Debug enabled
-        printf("PRINT-1: \n");
-        printPoints(A, 1);
-        printPoints(B, 1);
-#endif
       }
     }
   }
 
-#if DEBUG_ENABLE                    // Print Only if Debug enabled
-  printf("DIST: %f\n", dminCenter);
-#endif
   return dminCenter;
 }
 
@@ -130,14 +155,6 @@ float findClosestPair(point p[], long long size, point *A, point *B)
 
   point La, Lb, Ra, Rb, LRa, LRb;   // La & Lb - Smallest point on Left side, Ra & Rb - Smallest point on right side
 
-#if DEBUG_ENABLE                    // Print Only if Debug enabled
-  printf("findClosestPair: Size: %lld\n", size);
-  for(long long i = 0; i< size; i++)
-  {
-    printf("(%lld, %lld)\n", p[i].x, p[i].y);
-  }
-#endif
-
   if (size <= 3)
   {
     float dmin = calculateDistance(p[0], p[1]);     // Get initial min Distance
@@ -145,16 +162,6 @@ float findClosestPair(point p[], long long size, point *A, point *B)
 
     *A = p[0];          // Updte closest point every time we find a dmin
     *B = p[1];
-
-#if DEBUG_ENABLE                    // Print Only if Debug enabled
-    printf("PRINT-2: \n");
-    printPoints(A, 1);
-    printPoints(B, 1);
-#endif
-
-#if DEBUG_ENABLE                    // Print Only if Debug enabled
-    printf("BruteDist: %f\n", dmin);
-#endif
 
     for (long long i=1; i< size; i++)
     {
@@ -166,12 +173,6 @@ float findClosestPair(point p[], long long size, point *A, point *B)
           dmin = tempDist;
           *A = p[i];
           *B = p[j];
-
-#if DEBUG_ENABLE                    // Print Only if Debug enabled
-          printf("PRINT-3: \n");
-          printPoints(A, 1);
-          printPoints(B, 1);
-#endif
         }
       }
     }
@@ -189,29 +190,13 @@ float findClosestPair(point p[], long long size, point *A, point *B)
   {
     LRa = La;
     LRb = Lb;
-
-#if DEBUG_ENABLE                    // Print Only if Debug enabled
-    printf("PRINT-4: \n");
-    printPoints(&LRa, 1);
-    printPoints(&LRb, 1);
-#endif
   }
   else        // right is small
   {
     LRa = Ra;
     LRb = Rb;
-
-#if DEBUG_ENABLE                    // Print Only if Debug enabled
-    printf("PRINT-5: \n");
-    printPoints(&LRa, 1);
-    printPoints(&LRb, 1);
-#endif
   }
   findMinFlag = 0;  // Reset the flag
-
-#if DEBUG_ENABLE                    // Print Only if Debug enabled
-  printf("dminL: %f dminR: %f dminLR: %f\n", dminL, dminR, dminLR);
-#endif
 
   dminAll = findMin (dminLR, findMinInCenter(p, mid, dminLR, size, A, B), &findMinFlag);
 
@@ -219,12 +204,6 @@ float findClosestPair(point p[], long long size, point *A, point *B)
   {
     *A = LRa;
     *B = LRb;
-
-#if DEBUG_ENABLE                    // Print Only if Debug enabled
-    printf("PRINT-6: \n");
-    printPoints(A, 1);
-    printPoints(B, 1);
-#endif
   }
   return dminAll;
 }
@@ -248,6 +227,30 @@ long long getTotalLines(FILE *fp)
 
   return totalLines;
 }
+
+#if DIMENSION_3D	 // 3-Dimensional Enabled
+long long searchIndex(point p[], long long size, long long valX, long long valY, long long valZ)
+#else
+long long searchIndex(point p[], long long size, long long valX, long long valY)
+#endif
+{
+  long long i =0;
+
+  for(i = 0; i < size; i++)
+  {
+#if DIMENSION_3D	 // 3-Dimensional Enabled
+    if (valX == p[i].x && valY == p[i].y && valZ == p[i].z)
+#else
+    if (valX == p[i].x && valY == p[i].y)
+#endif
+    {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
 
 int main (int argc, char *argv[])
 {
@@ -274,11 +277,18 @@ int main (int argc, char *argv[])
 
   long long size = totalLines;
   point *p = malloc(totalLines * sizeof(point));
+  point *pOrg = malloc(totalLines * sizeof(point));
 
   for (long long i=0; i<size; i++)
   {
     fgets(string, 100, inputFD);
+#if DIMENSION_3D	 // 3-Dimensional Enabled
+    sscanf(string, "%lld %lld %lld", &(p[i].x), &(p[i].y), &(p[i].z));
+    sscanf(string, "%lld %lld %lld", &(pOrg[i].x), &(pOrg[i].y), &(pOrg[i].z));
+#else
     sscanf(string, "%lld %lld", &(p[i].x), &(p[i].y));
+    sscanf(string, "%lld %lld", &(pOrg[i].x), &(pOrg[i].y));
+#endif
   }
 
   fclose(inputFD);
@@ -290,26 +300,25 @@ int main (int argc, char *argv[])
     return -1;
   }
 
-#if DEBUG_ENABLE                    // Print Only if Debug enabled
+#if 0                    // Enable to see input
   printf("Input with Size: %lld -> [ ", size);
   for (long long i=0; i< size; i++)
+#if DIMENSION_3D	 // 3-Dimensional Enabled
+    printf("(%lld, %lld, %lld),", p[i].x, p[i].y, p[i].z);
+#else
     printf("(%lld, %lld),", p[i].x, p[i].y);
+#endif
   printf(" ]\n");
 #endif
 
   qsort(p, size, sizeof( point), qsortCompX);         // Sort the array with respect to X - coordinate
-
-#if DEBUG_ENABLE                    // Print Only if Debug enabled
-  printf("After X Sort\n");
-  for(long long i = 0; i< size; i++)
-  {
-    printf("(%lld, %lld)\n", p[i].x, p[i].y);
-  }
-#endif
-
   minDistance = findClosestPair(p, size, &A, &B);
 
-  fprintf(outputFD, "(%lld, %lld) (%lld, %lld) %f\n", A.x, A.y, B.x, B.y, minDistance);       // Write the output to the file
+#if DIMENSION_3D	 // 3-Dimensional Enabled
+  fprintf(outputFD, "%lld %lld %f\n", searchIndex(pOrg, size, A.x, A.y, A.z), searchIndex(pOrg, size, B.x, B.y, B.z), minDistance);       // Write the output to the file
+#else
+  fprintf(outputFD, "%lld %lld %f\n", searchIndex(pOrg, size, A.x, A.y), searchIndex(pOrg, size, B.x, B.y), minDistance);       // Write the output to the file
+#endif
 
   fclose(outputFD);
   free(p);
